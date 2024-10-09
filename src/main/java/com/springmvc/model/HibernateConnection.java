f062013c562c90c2a3f585a7a6bac552232f4a5b
F1nn-ach <fsl.finscarlett@gmail.com>
2024-10-09 15:49:53 +0700

    Update HibernateConnection.java

--- src/main/java/com/springmvc/model/HibernateConnection.java
+++ src/main/java/com/springmvc/model/HibernateConnection.java
@@ -8,9 +8,9 @@ import org.hibernate.cfg.Configuration;
  
 public class HibernateConnection {
 	public static SessionFactory sessionFactory;
-	static String url = "jdbc:mysql://localhost:33091/it311?characterEncoding=UTF-8"; 
-	static String uname = "project";
-	static String pwd = "password";
+	static String url = "jdbc:mysql://localhost:3306/it311?characterEncoding=UTF-8"; 
+	static String uname = "root";
+	static String pwd = "1234";
 	public static SessionFactory doHibernateConnection(){
 		Properties database = new Properties();
 		database.setProperty("hibernate.hbm2ddl.auto", "update"); //หลังจากสร้างตารางแล้วให้เอาออก
